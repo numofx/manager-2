3a78178037014b7990f53a6d96d9945180ca6c6a
Robert Leifke <r.leifke@gmail.com>
2026-01-14 00:15:38 +0300

    feat: update forge-std script imports

--- script/DeployMinimalCeloSystem.s.sol
+++ script/DeployMinimalCeloSystem.s.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity >=0.8.13;
 
-import "forge-std/src/Script.sol";
+import "forge-std/Script.sol";
 import "../src/Cauldron.sol";
 import "../src/Ladle.sol";
 import "../src/Join.sol";
